829fa06b7b57ba0e6c579f315c33eceee1cf561a
Prudvi <prudvirajvarma2004A=@gmail.com>
2024-12-11 22:06:51 +0530

    added routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
+import React from 'react'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import Send from "./components/Send.jsx"
+import Store from "./components/Store.jsx"
+import View from "./components/View.jsx"
+import Verify from "./components/Verify.jsx"
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element: <App/>
+  },
+  {
+    path : "/send",
+    element : <Send/>
+  },
+  {
+    path : "/store",
+    element : <Store/>
+  },
+  {
+    path : "/view",
+    element : <View/>
+  },
+  {
+    path : "/verify",
+    element : <Verify/>
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
