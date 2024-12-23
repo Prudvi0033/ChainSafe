c96c65327904b5b85a48e3bb833720f4ee92e046
Prudvi <prudvirajvarma2004A=@gmail.com>
2024-12-23 21:51:24 +0530

    Added responsivness to view

--- client/src/pages/DisplayDocs.jsx
+++ client/src/pages/DisplayDocs.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 const DisplayDocs = ({ documents, isLoading }) => {
   if (isLoading) {
-    return <div className="p-4">Loading document data...</div>;
+    return <div className="w-full h-screen text-center gradient-bg-transactions text-white text-4xl">Loading document data...</div>;
   }
 
   if (!documents || documents.length === 0) {
-    return <div className="p-4">No document data available</div>;
+    return <div className="w-full h-screen text-center gradient-bg-transactions text-white text-4xl">No document data available</div>;
   }
 
   return (
-    <div className="p-4 bg-gray-100 rounded shadow-md">
-      <h2 className="text-lg font-bold mb-2">Document Details</h2>
+    <div className="text-white w-full p-7 flex flex-col shadow-md gradient-bg-transactions">
+      <h2 className="font-bold mb-4 text-center text-4xl text-gradient">Document Details</h2>
       {documents.map((document, index) => (
-        <div key={index} className="mb-4 p-4 bg-white rounded shadow">
+        <div key={index} className="mb-4 break-words p-2 lg:p-4 w-full white-glassmorphism rounded shadow">
           <div className="mb-2">
             <strong>Document Name:</strong>
             <p>{document.Document_Name || 'N/A'}</p>
