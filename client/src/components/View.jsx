0436e0bb01671a6a9c5be9bd9aaa9f413669f10f
Prudvi <prudvirajvarma2004A=@gmail.com>
2024-12-23 21:12:08 +0530

    added chnages to view

--- client/src/components/View.jsx
+++ client/src/components/View.jsx
@@ -1,5 +1,5 @@
 import { Client, Databases, ID } from "appwrite";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import DisplayDocs from "../pages/DisplayDocs";
 
 const client = new Client();
@@ -8,34 +8,37 @@ client.setProject('67695e4200020f68eb7b');
 const databases = new Databases(client);
 
 const View = () => {
-
   const [documents, setDocuments] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchDocs = () => {
-    setIsLoading(true)
+    setIsLoading(true);
+    setError(null);  
     databases
-    .listDocuments('67695f7700218d76597c', '67695f8b0010ccc83277')
-    .then((response) => {
-      setDocuments(response.documents)
-      setIsLoading(false)
-    })
-    .catch((error) => {
-      console.error(error);
-      setIsLoading(false);
-    });
-  }
+      .listDocuments('67695f7700218d76597c', '67695f8b0010ccc83277')
+      .then((response) => {
+        setDocuments(response.documents);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError('Failed to fetch documents.');
+        setIsLoading(false);
+      });
+  };
 
-  useEffect(()=>{
-    fetchDocs()
-  },[])
+  useEffect(() => {
+    fetchDocs();
+  }, []);
 
   return (
     <div>
       <h2>Documents in Another Component</h2>
+      {error && <p className="text-red-500">{error}</p>}
       <DisplayDocs documents={documents} isLoading={isLoading} />
     </div>
-  )
-}
+  );
+};
 
 export default View;
